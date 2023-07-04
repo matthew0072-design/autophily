f05db9b7705cc4fdf4907e6cf7b4ebc0adba5c75
mattscript <ayobamifagoroye@yahoo.com>
2023-07-04 03:49:04 +0100

    earphone file

--- src/app/earphones/[id]/page.tsx
+++ src/app/earphones/[id]/page.tsx
@@ -44,7 +44,7 @@ const relatedProducts = allProducts.filter((product:any) => product.id  !== curr
 
 
 // Function to get a specified number of random elements from an array
-function getRandomElements(array:any, numElements:any) {
+function getRandomElements(array:string[] | number[] , numElements:number) {
     const shuffledArray = array.sort(() => 0.5 - Math.random());
     return shuffledArray.slice(0, numElements);
   }
