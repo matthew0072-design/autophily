659c24d2536e8839d01a375874c51288691e4b93
mattscript <ayobamifagoroye@yahoo.com>
2023-07-04 11:14:33 +0100

    changing localhost

--- src/app/earphones/[id]/page.tsx
+++ src/app/earphones/[id]/page.tsx
@@ -8,7 +8,7 @@ import Audiogear from '../../components/Audiogear'
 // to get  static path using product id
 export async function generateStaticParams() {
 
-    const response = await fetch('http://localhost:3000/headphones/api/')
+    const response = await fetch('http://127.0.0.1:3000/headphones/api/')
     const productName = await response.json()
  
 
@@ -30,7 +30,7 @@ export default async function Page ({ params } : { params: { id:number }}) {
     
   
 
-    const productResponse = await fetch('http://localhost:3000/headphones/api/')
+    const productResponse = await fetch('http://127.0.0.1/headphones/api/')
     const allProducts = await productResponse.json()
     
     const currentProduct = allProducts.find((product:any) => product.id === Number(params.id))
@@ -64,7 +64,7 @@ const productsYouMayLike = getRandomElements(relatedProducts, 3);
 
     
     //To fetch a single product by id
-    const productDetails = await fetch(`http://localhost:3000/headphones/api/eachPage/?id=${params.id}`)
+    const productDetails = await fetch(`http://127.0.0.1:3000/headphones/api/eachPage/?id=${params.id}`)
 
     const product = await productDetails.json()
 
